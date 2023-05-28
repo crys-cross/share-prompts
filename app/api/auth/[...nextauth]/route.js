aaa2d56b248db61978d4baaedf1b71cf06793595
Crys <mariano.christopher123@gmail.com>
2023-05-28 13:01:43 +0800

    edited api and database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,44 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/provider/google";
 import { signIn } from "next-auth/react";
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: "",
-      cleintSecret: "",
+      clientId: process.env.GOOGLE_ID,
+      cleintSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+      //check if the user already exist
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      // if not, create a new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
