bc27a6e5b1f64ee6ce06779a70b48fa625b39f18
Crys <mariano.christopher123@gmail.com>
2023-05-29 10:05:55 +0800

    updated login function and database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/provider/google";
+import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
@@ -11,33 +11,35 @@ const handler = NextAuth({
       cleintSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-      //check if the user already exist
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        //check if the user already exist
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const CreatePrompt = () => {
+  return <div>CreatePrompt</div>;
+};
+
+export default CreatePrompt;
